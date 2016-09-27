07ed92e98ff92951209e3b673ed9fffc157b6823
jinimcoroneo <jinim1983@gmail.com>
2016-09-27 17:06:27 +0100

    Challenge 1 - 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
-describe "docking_station" do
-  it "describes a docking station" do
-    expect docking_station.to eq "this is a docking station"
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+  #it "should respond to release bike" do  
+    #expect(DockingStation).to respond_to(:release_bike) -- what is the proper syntax for the long version?
 end
-end
+
